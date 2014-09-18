bcf682f6a4b85b2d7a6b668be86e79d9ee30ae1f
mjbrown <mjbrown.droid@gmail.com>
2014-09-18 01:03:02 -0500

    Make the code poorly structured, impossible to test, and difficult to re-use.

--- yasp.c
+++ yasp.c
@@ -3,8 +3,9 @@
 #include <stdint.h>
 #include <stdio.h>
 
-#include "i2c_HAL.h"
-#include "serial_HAL.h"
+#include <usb/usb.h>
+#include <usb/usb_device_cdc.h>
+#include <i2c.h>
 
 #define SYNCH_BYTE  0xFF
 
@@ -23,6 +24,50 @@ uint16_t BigEndianArrayToUint16(uint8_t * array)
 #define I2CRD_CTRLBYTE_POS  2
 #define I2CRD_LEN_POS       3
 #define I2CRD_CMDDATA_POS   5
+
+uint8_t I2C_Write(uint8_t controlByte, uint8_t * data, uint16_t length)
+{
+    uint16_t i;
+    IdleI2C();
+    StartI2C();
+    while ( SSPCON2bits.SEN );
+    WriteI2C(controlByte);
+    IdleI2C();
+    for (i = 0; i < length; i++)
+    {
+        WriteI2C(data[i]);
+        IdleI2C();
+    }
+    StopI2C();
+    while ( SSPCON2bits.PEN );
+    return 0;
+}
+
+uint8_t I2C_Read(uint8_t controlByte, uint8_t * cmd, uint16_t cmd_len,
+        uint8_t * data, uint16_t data_len)
+{
+    uint16_t i;
+    IdleI2C();
+    StartI2C();
+    while (SSPCON2bits.SEN);
+    WriteI2C(controlByte);
+    IdleI2C();
+    for (i = 0; i < cmd_len; i++)
+    {
+        WriteI2C(cmd[i]);
+        IdleI2C();
+    }
+    RestartI2C();
+    while (SSPCON2bits.RSEN);
+    WriteI2C(controlByte | 0x01);
+    IdleI2C();
+    getsI2C(data, data_len);
+    NotAckI2C();
+    while (SSPCON2bits.ACKEN);
+    StopI2C();
+    while (SSPCON2bits.PEN);
+    return 0;
+}
 
 int16_t executeCommand(uint8_t cmd, uint8_t * payload, uint8_t * ack_msg, uint16_t * ack_length)
 {
@@ -98,6 +143,23 @@ void yasp_Init()
     for (buffer_len = 0; buffer_len < 64; buffer_len++)
         cmd_buffer[buffer_len] = 0;
     buffer_len = 0;
+    // I2C Initialization
+    TRISBbits.TRISB0 = 0;
+    TRISBbits.TRISB1 = 0;
+    ADCON1bits.PCFG = 0xF;
+    SSPADD = 0x3F;
+    OpenI2C(MASTER, SLEW_OFF);
+
+}
+
+void serial_tx(uint8_t * data, uint16_t length)
+{
+    if(mUSBUSARTIsTxTrfReady() == true)
+    {
+        putUSBUSART(data, length);
+        return;
+    }
+    return;
 }
 
 void _resynch(uint16_t offset)
@@ -119,7 +181,10 @@ void yaspService()
     //return;
     uint16_t i = 1;
     int16_t return_code = 0;
-    buffer_len += serial_rx(cmd_buffer + buffer_len);
+    if( USBUSARTIsTxTrfReady() == true)
+    {
+        buffer_len += getsUSBUSART(cmd_buffer + buffer_len, 64);
+    }
     if (buffer_len > 0)
     {
         return_code = processCommand(cmd_buffer, buffer_len, ack_buffer, &ack_len);
