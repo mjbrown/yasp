dad3a0971756fc56c0c488b208fa853ddab9b54d
Michael Brown <mbrown@intricon.com>
2018-06-13 14:38:48 -0500

    Start from scratch. Empty project generated by CLion.

---
+++ cmake-build-debug/CMakeFiles/yasp.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/yasp.dir/main.c.obj"
+  "yasp.pdb"
+  "yasp.exe"
+  "yasp.exe.manifest"
+  "libyasp.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/yasp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- crc32/crc32.h
+++
@@ -1,8 +0,0 @@
-/*-
- *  COPYRIGHT (C) 1986 Gary S. Brown.  You may use this program, or
- *  code or tables extracted from it, as desired without restriction.
- */
-
-uint32_t crc32(uint32_t crc, const void *buf, size_t size);
-void crc32_serialize(uint32_t crc, const void *buf);
-uint32_t crc32_deserialize(const void *buf);
